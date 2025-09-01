fe01f6ed2f8de5e96094826f8fe8365be13dd83f
HyeonsuLee <leehyeonsu4888@naver.com>
2025-09-01 22:19:09 +0900

    chore: 의존성 최적화

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    id("java")
+    id("java-library")
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.vanniktech.maven.publish") version "0.28.0"
@@ -9,22 +9,38 @@ plugins {
     signing
 }
 group = "io.github.20hyeonsulee"
-version = "1.0.0"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    
+    // 사용자 프로젝트에서 제공될 것으로 기대하는 의존성
+    compileOnly("org.springframework:spring-messaging")
+    compileOnly("org.springframework:spring-websocket") 
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+
+    // 내부 구현용 - 버전을 고정하되 사용자에게 노출되지 않음
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.0")
     implementation("org.reflections:reflections:0.10.2")
     implementation("com.github.victools:jsonschema-generator:4.37.0")
-    implementation("org.springframework:spring-messaging:6.2.8")
-    implementation("org.springframework:spring-websocket:6.2.8")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.yaml:snakeyaml:2.0")
+}
+
+// 라이브러리이므로 bootJar 비활성화
+tasks.bootJar {
+    enabled = false
+}
+
+tasks.jar {
+    enabled = true
+    archiveClassifier = ""
 }
 
 signing {
@@ -35,11 +51,11 @@ signing {
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-    coordinates("io.github.20hyeonsulee", "websocket-docs-generator", "1.0.2")
+    coordinates("io.github.20hyeonsulee", "websocket-docs-generator", "1.0.4")
 
     pom {
         name = "WebSocket Docs Generator"
-        description = "A Kotlin library for generating WebSocket API documentation"
+        description = "A Java library for generating WebSocket API documentation"
         inceptionYear = "2024"
         url = "https://github.com/20HyeonsuLee/websocket-docs-generator"
 
